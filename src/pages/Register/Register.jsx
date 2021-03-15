2fccbb2ede2942bb7885408ca546f5e17ba4a8e8
Dustin <59592287+DustinOnTheMIC@users.noreply.github.com>
2021-03-15 15:30:11 +0700

    update axios link

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -82,7 +82,7 @@ class Register extends Component {
     }
 
     async callAPI (input) {  
-        await axios.post('http://127.0.0.1:8000/api/v1/auth/register', input).then(resp => {
+        await axios.post('http:dichvuthucung.herokuapp.com/api/v1/auth/register', input).then(resp => {
             let token = resp.data.data.access_token
             if(token.length !== 0) {
                 localStorage.setItem('token', token)
